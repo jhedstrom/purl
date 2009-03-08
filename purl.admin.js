fa70d0e998a6c41c7154f339d6cae577c073013f
young hahn <yhahn@264833.no-reply.drupal.org>
2009-03-08 23:39:24 +0000

    Adding Persistent URL module (PURL). Abstracts custom_url_rewrite() handling into an API that multiple modules can take advantage of. Renamed/improved version of context_prefix from the context project.

---
+++ purl.admin.js
@@ -0,0 +1,28 @@
+// $Id$
+
+if (typeof(Drupal) == "undefined" || !Drupal.purl_admin) {
+  Drupal.purl_admin = {};
+}
+
+Drupal.purl_admin.attach = function() {
+  $('select[id^="edit-purl-"]:not(.purl-processed)').change(function(i) {
+    Drupal.purl_admin.alter(this);
+  }).each(function(i){
+    Drupal.purl_admin.alter(this);
+  }).addClass('purl-processed');
+}
+
+Drupal.purl_admin.alter = function(elem){
+  if (elem.value == 'pair') {
+    $(elem).parents('tr').find('input[id^="edit-purl-"]').show();
+  }
+  else {
+    $(elem).parents('tr').find('input[id^="edit-purl-"]').hide();
+  }
+}
+
+Drupal.behaviors.purl = function() {
+  if ($('form#purl-settings-form').size() > 0) {
+    Drupal.purl_admin.attach();
+  }
+}
